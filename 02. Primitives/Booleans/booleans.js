51993aeef92a67779d3678e83dca73994791f113
Umer Shaikh <umershaikh1444@gmail.com>
2025-08-20 10:12:13 +0530

    new folder

---
+++ 02. Primitives/Booleans/booleans.js
@@ -0,0 +1,41 @@
+//Boolean
+let isLoggedIn = false;
+
+if (isLoggedIn) {
+  console.log("Welcome back user!");
+} else {
+  console.log("please log in.");
+}
+
+//NAN
+let numbers = 10;
+console.log(numbers + undefined);
+
+//undefined
+
+let userName;
+console.log(userName);
+
+//null
+let userScore = null;
+console.log(userScore);
+
+// falsy Values
+// null
+// undefined
+// 0
+// -0
+// NaN
+// '', "", ``, (empty strings)
+
+// 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
+// 2. Create variable name (isJsHard) & store false as a value.
+// 3. Create variable name (favNumb) & store your favorite number inside.
+// 4. Now Add favNumber with the value of undefined.
+
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 18;
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb + undefined);
---
+++ 02. Primitives/numbers/num.js
@@ -0,0 +1,38 @@
+// 1. Create Variable name (firstFavNum) & store your favorite number.
+// 2. Create Variable name (secondFavNum) & store your second fav number.
+// 3. Add (firstFavNum & secondFavNumber).
+// 4. Subtract (firstFavNum & secondFavNumber).
+// 5. Multiply (firstFavNum & secondFavNumber).
+// 6. Divided (firstFavNum & secondFavNumber).
+// 7. Check (firstFavNum Mod secondFavNumber)
+// 8. Check the power of (firstFavNum)
+
+//let firstFavNum = 18;
+//let secondFavNum = 17;
+//console.log(firstFavNum + secondFavNum); // Addition
+//console.log(firstFavNum - secondFavNum); // Subtraction
+//console.log(firstFavNum * secondFavNum); // Multiplication
+//console.log(firstFavNum / secondFavNum); // Division
+//console.log(firstFavNum % secondFavNum); // Modulus
+//console.log(firstFavNum ** secondFavNum); // Exponentiation
+
+// JavaScript has one number type
+// Positive Number
+// Negative Number
+// Whole Number (integers)
+// Decimal Numbers
+
+const randomNumber = 10200;
+const favNumber = 21;
+let num = 2;
+
+// Basic Math
+console.log(randomNumber * favNumber);
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+console.log(num++);
+console.log(num--);
